855e32b37c740f81a5d3c25d00e6185c546ddbf8
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-06-17 23:10:34 +0200

    add ssh keys

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,12 +15,18 @@ terraform {
   }
 }
 
-variable "HCLOUD_TOKEN" {
-  sensitive = true
-}
-
 provider "hcloud" {
   token = var.HCLOUD_TOKEN
+}
+
+resource "hcloud_ssh_key" "home_desktop" {
+  name = "home_desktop"
+  public_key = var.SSH_KEY_PUBLIC_HOME_DESKTOP
+}
+
+resource "hcloud_ssh_key" "github_actions" {
+  name = "github_actions"
+  public_key = var.SSH_KEY_PUBLIC_GITHUB_ACTIONS
 }
 
 resource "hcloud_server" "aquila" {
@@ -28,6 +34,7 @@ resource "hcloud_server" "aquila" {
   image       = "ubuntu-24.04"
   server_type = "cx22"
   location    = "nbg1"
+  ssh_keys    = ["home_desktop", "github_actions"]
   public_net {
     ipv4_enabled = true
     ipv6_enabled = true
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "HCLOUD_TOKEN" {
+  sensitive = true
+}
+
+variable "SSH_KEY_PUBLIC_HOME_DESKTOP" {
+  sensitive = true
+}
+
+variable "SSH_KEY_PUBLIC_GITHUB_ACTIONS" {
+  sensitive = true
+}
