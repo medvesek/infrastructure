5f40e7b998353543a4d422cd579ecbe1127e3a65
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-06-19 23:06:00 +0200

    mail

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -25,3 +25,30 @@ resource "cloudflare_dns_record" "testme" {
   proxied = true
   ttl = 1
 }
+
+resource "cloudflare_dns_record" "mail" {
+  zone_id = cloudflare_zone.cmrlj.id
+  name = "mail"
+  type = "A"
+  content = hcloud_server.aquila.ipv4_address
+  proxied = true
+  ttl = 1
+}
+
+
+resource "cloudflare_dns_record" "mail_mx" {
+  zone_id = cloudflare_zone.cmrlj.id
+  name = "@"
+  type = "mx"
+  content = "mail.cmrlj.eu"
+  proxied = true
+  ttl = 1
+}
+
+resource "cloudflare_dns_record" "mail_spf" {
+  zone_id = cloudflare_zone.cmrlj.id
+  name = "v=spf1 a mx ~all"
+  type = "txt"
+  proxied = true
+  ttl = 1
+}
