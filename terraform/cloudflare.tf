fff59006c8b43ad9b646ee794e3ee1fb8c352770
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-06-21 22:08:08 +0200

    cloudflare

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -41,7 +41,6 @@ resource "cloudflare_dns_record" "mail" {
   ttl = 1
 }
 
-
 resource "cloudflare_dns_record" "mail_mx" {
   zone_id = cloudflare_zone.cmrlj.id
   name = "cmrlj.eu"
@@ -54,6 +53,32 @@ resource "cloudflare_dns_record" "mail_mx" {
 resource "cloudflare_dns_record" "mail_spf" {
   zone_id = cloudflare_zone.cmrlj.id
   name = "cmrlj.eu"
+  content = "\"v=spf1 a mx ~all\""
+  type = "TXT"
+  ttl = 3600
+}
+
+resource "cloudflare_dns_record" "ajmuht_mail" {
+  zone_id = data.cloudflare_zone.ajmuht.zone_id
+  name = "mail.ajmuht.eu"
+  type = "A"
+  content = hcloud_server.aquila.ipv4_address
+  proxied = true
+  ttl = 1
+}
+
+resource "cloudflare_dns_record" "ajmuht_mail_mx" {
+  zone_id = data.cloudflare_zone.ajmuht.zone_id
+  name = "ajmuht.eu"
+  type = "MX"
+  content = "mail.ajmuht.eu"
+  priority = 1
+  ttl = 1
+}
+
+resource "cloudflare_dns_record" "ajmuht_mail_spf" {
+  zone_id = data.cloudflare_zone.ajmuht.zone_id
+  name = "ajmuht.eu"
   content = "\"v=spf1 a mx ~all\""
   type = "TXT"
   ttl = 3600
