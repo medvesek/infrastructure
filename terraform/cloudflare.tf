5392a8471cd0f8d84823a03ea5edaab6e6dbab84
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-06-22 23:09:09 +0200

    cloudflare

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -14,12 +14,32 @@ resource "cloudflare_zone" "ajmuht" {
     id = data.cloudflare_account.my_account.account_id
   }
 }
+resource "cloudflare_zone_setting" "ajmuht_ssl_automatic_mode" {
+  zone_id = cloudflare_zone.ajmuht.id
+  setting_id = "ssl_automatic_mode"
+  value = "custom"
+}
+resource "cloudflare_zone_setting" "ajmuht_ssl_mode" {
+  zone_id = cloudflare_zone.ajmuht.id
+  setting_id = "ssl"
+  value = "full"
+}
 resource "cloudflare_zone" "cmrlj" {
   name = "cmrlj.eu"
   type = "full"
   account ={
     id = data.cloudflare_account.my_account.account_id
   } 
+}
+resource "cloudflare_zone_setting" "cmrlj_ssl_automatic_mode" {
+  zone_id = cloudflare_zone.cmrlj.id
+  setting_id = "ssl_automatic_mode"
+  value = "custom"
+}
+resource "cloudflare_zone_setting" "cmrlj_ssl_mode" {
+  zone_id = cloudflare_zone.cmrlj.id
+  setting_id = "ssl"
+  value = "full"
 }
 
 // DNS - SITES
