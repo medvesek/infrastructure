21c74a585e01968d6c7295ee0bea822106102ace
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-02 16:27:00 +0200

    terraform

--- terraform/cloudflare/mail.tf
+++ terraform/cloudflare/mail.tf
@@ -9,36 +9,3 @@ module "ajmuht_mail" {
   zone = cloudflare_zone.ajmuht
   ip = var.servers.aquila
 }
-
-
-
-moved {
-  from = cloudflare_dns_record.ajmuht_mail
-  to = module.ajmuht_mail.cloudflare_dns_record.a
-}
-
-moved {
-  from = cloudflare_dns_record.ajmuht_mail_mx
-  to = module.ajmuht_mail.cloudflare_dns_record.mx
-}
-
-moved {
-  from = cloudflare_dns_record.ajmuht_mail_spf
-  to = module.ajmuht_mail.cloudflare_dns_record.spf
-}
-
-
-moved {
-  from = cloudflare_dns_record.cmrlj_mail
-  to = module.cmrlj_mail.cloudflare_dns_record.a
-}
-
-moved {
-  from = cloudflare_dns_record.cmrlj_mail_mx
-  to = module.cmrlj_mail.cloudflare_dns_record.mx
-}
-
-moved {
-  from = cloudflare_dns_record.cmrlj_mail_spf
-  to = module.cmrlj_mail.cloudflare_dns_record.spf
-}
--- terraform/cloudflare/sites.tf
+++ terraform/cloudflare/sites.tf
@@ -25,24 +25,3 @@ module "site_laravel_example_ajmuht" {
   ip = var.servers.aquila
   hostname = "laravel-example.ajmuht.eu"
 }
-
-
-moved {
-  from = cloudflare_dns_record.test_ajmuht
-  to = module.site_test_ajmuht.cloudflare_dns_record.a
-}
-
-moved {
-  from = cloudflare_dns_record.test_cmrlj
-  to = module.site_test_cmrlj.cloudflare_dns_record.a
-}
-
-moved {
-  from = cloudflare_dns_record.traefik_ajmuht
-  to = module.site_traefik_ajmuht.cloudflare_dns_record.a
-}
-
-moved {
-  from = cloudflare_dns_record.laravel_example
-  to = module.site_laravel_example_ajmuht.cloudflare_dns_record.a
-}
