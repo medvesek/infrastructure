65ba0460b87ef1de927bbf74ba9c615f146fdca8
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-03 00:09:56 +0200

    terraform

--- terraform/cloudflare/zones.tf
+++ terraform/cloudflare/zones.tf
@@ -8,34 +8,3 @@ module "cmrlj_eu_domain" {
   account_id = data.cloudflare_account.my_account.account_id
   domain = "cmrlj.eu"
 }
-
-moved {
-  from = cloudflare_zone.ajmuht
-  to = module.ajmuht_eu_domain.cloudflare_zone.zone
-}
-
-moved {
-  from = cloudflare_zone.cmrlj
-  to = module.cmrlj_eu_domain.cloudflare_zone.zone
-}
-
-moved {
-  from = cloudflare_zone_setting.ajmuht_ssl_automatic_mode
-  to = module.ajmuht_eu_domain.cloudflare_zone_setting.ssl_automatic_mode
-}
-
-moved {
-  from = cloudflare_zone_setting.ajmuht_ssl_mode
-  to = module.ajmuht_eu_domain.cloudflare_zone_setting.ssl_mode
-}
-
-moved {
-  from = cloudflare_zone_setting.cmrlj_ssl_automatic_mode
-  to = module.cmrlj_eu_domain.cloudflare_zone_setting.ssl_automatic_mode
-}
-
-moved {
-  from = cloudflare_zone_setting.cmrlj_ssl_mode
-  to = module.cmrlj_eu_domain.cloudflare_zone_setting.ssl_mode
-}
-
