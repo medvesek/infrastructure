a2ecf3a2a49c70dbb13e645309d90ab1e360a4ac
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-07 07:07:26 +0200

    terraform

--- terraform/cloudflare/dns.tf
+++ terraform/cloudflare/dns.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_dns_record" "freeunlimitedparking_ajmuht_cname" {
   zone_id = module.ajmuht_eu_domain.id
-  name = "freeunlimitedparking.ajmiht.eu"
+  name = "freeunlimitedparking.ajmuht.eu"
   type = "CNAME"
   content = "medvesekg.github.io"
   proxied = true
