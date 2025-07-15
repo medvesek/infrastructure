fc019cc4dbd64470662abcc30a2325138f94df9d
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-15 12:02:12 +0200

    remove unneeded cname

--- terraform/cloudflare/dns.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_dns_record" "freeunlimitedparking_ajmuht_cname" {
-  zone_id = module.ajmuht_eu_domain.id
-  name = "freeunlimitedparking.ajmuht.eu"
-  type = "CNAME"
-  content = "medvesekg.github.io"
-  proxied = true
-  ttl = 1
-}
